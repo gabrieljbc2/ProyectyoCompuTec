a887a5a7807d1c59720fe62fd50d4f8432f50e1f
gabrieljbc2 <gabrieljbc2@gmail.com>
2022-04-02 21:33:15 -0600

    Prueba 2

---
+++ src/test/java/ProyectoCompuTec/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoCompuTec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
